5c179252f8841d0920cbcd03f4fee6cc0fc7ae50
shakshi2003 <pragatisrivastava2800@gmail.com>
2024-09-02 23:46:01 +0530

    first commit

---
+++ API/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from 'express'
+import userRouter from './routes/user.js'
+import recipeRouter from './routes/recipe.js'
+import cors from 'cors'
+const app = express(); 
+
+app.use(bodyParser.json())
+app.use(cors({
+  origin:true,
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+ 
+}))
+
+// userRouter
+app.use('/api',userRouter)
+
+// recipeRouter
+app.use('/api',recipeRouter)
+
+mongoose
+  .connect(
+    "mongodb+srv://shakshi040403:Manjula@cluster0.xiv2k.mongodb.net/",
+    {
+      dbName: "MERN_Recipe",
+    }
+  )
+  .then(() => console.log("MongoDB is Connected..!"))
+  .catch((err) => console.log(err.message));
+
+const port = 3000;
+app.listen(port, () => console.log(`server is running on port ${port}`));
+
